2cbcda359758ed493ce32bed2743e81e3248c951
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2020-08-29 09:49:47 +0000

    index.js updated on 2020-08-29T09:49:47Z

--- index.js
+++ index.js
@@ -363,7 +363,7 @@ var confirmQueueMove = function(hash){
 	}
 }
 var confirmQueuePush = function(command,perm){
-	confirmQueue.push({cmd:command,perm:perm})
+	confirmQueue.push(command,perm})
 	cwc(csl[0]+"Are you sure you want to run \""+csl[1]+command+csl[0]+"? Type "+csl[1]+"|confirm <CODE>"+csl[0]+" to confirm.")
 }
 
