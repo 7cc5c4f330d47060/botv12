26729087fb37f5669c9d45b5da0811aca8909213
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2020-08-29 03:05:01 +0000

    index.js updated on 2020-08-29T03:05:01Z

--- index.js
+++ index.js
@@ -289,9 +289,9 @@ var command=function(n,d){
 			//console.log("Correct permission ("+n+"): "+commands[c.split(" ")[0]])
 			if(getAdmin(n)>=commands[c.split(" ")[0]].admin){
 				//console.log("Correct admin permission ("+n+"): "+commands[c.split(" ")[0]])
-				if(NoCommands || getAdmin(n)>=1){
+				//if(NoCommands || getAdmin(n)>=1){
 					commands[c.split(" ")[0]].command(d,n);
-				}
+				//}
 			}
 		}
 	}
