8c16fc8d08aea6850291c843435cf39790987ed6
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2020-09-12 19:17:58 +0000

    index.js updated on 2020-09-12T19:17:58Z

--- index.js
+++ index.js
@@ -509,7 +509,7 @@ client.on('player_info', function(packet) {
 		}
 		if(packet.action==1){
 			if(packet.data[i1c].gamemode){
-			console.log(csl[1]+p[packet.data[i1c].UUID].name+csl[0]+" went from "+csl[1]+gamemodes[p[packet.data[i1c].UUID].gamemode]+csl[0]+" Mode to "+csl[1]+gamemodes[packet.data[i1c].gamemode]+csl[0]+" Mode!")
+			console.log(csl[1]+packet.data[i1c].UUID+csl[0]+" went from "+csl[1]+gamemodes[p[packet.data[i1c].UUID].gamemode]+csl[0]+" Mode to "+csl[1]+gamemodes[packet.data[i1c].gamemode]+csl[0]+" Mode!")
 			p[packet.data[i1c].UUID].gamemode=packet.data[i1c].gamemode;
 			}
 		}
