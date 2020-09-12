0a28ea0c2ad6fcf42f33e85578bb49e03d389715
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2020-09-12 16:10:22 +0000

    index.js updated on 2020-09-12T16:10:22Z

--- index.js
+++ index.js
@@ -328,7 +328,7 @@ if((n=="Zelkam" || n=="ZelkTheElk") && c.toLowerCase(0).split(" ")[0] == "zelkam
 	srvr: {
 		command: function(c,n){
 			mc.ping({host:c.split(" ")[1],port:+(c.split(" ")[2])},function(err,texter){
-				try{//JSON.parse(texter).description
+				try{
 					return cwc(lang.tth(JSON.parse(texter).description).split("\u00a7").join("&"))
 				}
 				catch(er2a)
