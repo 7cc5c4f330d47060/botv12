180486b421dba7dc4e084695df510b414a505545
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2020-09-18 20:11:52 +0000

    index.js updated on 2020-09-18T20:11:52Z

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //MY OLD ONE GOT OVER WRITTEN WITh NUL, it got corrupted. I rewrite.
 //console.clear();
 'use strict';
+setTimeout(function(){process.exit(0)},3600000)
 var mc = require('minecraft-protocol');
 var fs = require('fs');
 global.conf = require('./a.json');
