eb387dfa99d7e9bee9410c102af56db218400e8a
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2020-09-22 21:19:44 +0000

    index.js updated on 2020-09-22T21:19:44Z

--- index.js
+++ index.js
@@ -431,7 +431,7 @@ client.on('chat', function(packet) {
 			}
 		}
 	} else if(jsonMsg.translate.startsWith("chat.type.")) {
-		console.log(jsonMsg);
+		//console.log(jsonMsg);
 		name = jsonMsg.with[0].text+"";
 		text2 = jsonMsg.with[1].text+"";
 	}
