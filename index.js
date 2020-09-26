0a4fae0b7d3ed1f666326d8af7befdf0972a3f1d
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2020-09-26 12:44:46 +0000

    index.js updated on 2020-09-26T12:44:46Z

--- index.js
+++ index.js
@@ -152,7 +152,6 @@ var packetc=60;
 	  
 	  if(packetc<=0){process.exit(0)} else if(packetc<=35){console.log(packetc)}
   },1000)
-global.doCommands();
 
 for(var i1b in global.commands){
 	global.cmdid.push({name:i1b,h:commands[i1b].h,usage:commands[i1b].u})
