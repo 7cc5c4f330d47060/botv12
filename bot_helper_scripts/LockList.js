f2dbca9c7dbb0e32913ca863a14b23fa88cf1222
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2020-09-27 17:35:11 +0000

    bot_helper_scripts/LockList.js updated on 2020-09-27T17:35:11Z

---
+++ bot_helper_scripts/LockList.js
@@ -0,0 +1,52 @@
+var fs = require("fs");
+class LockList {
+	static add(user){
+		const data = fs.readFileSync('../locked.json', 'utf8');
+		fs.unlink("../locked.json");
+		var locked=JSON.parse(data);
+		for(var i in locked){
+			if(i.uuid==user.uuid){
+				return;
+			}
+		}
+		locked[user.name]=user.uuid;
+		try {
+			const data = fs.writeFileSync('../locked.json', JSON.stringify(locked));
+		} catch (err) {
+			console.error(err)
+		}
+		return user.uuid;
+	}
+	static remove(user){
+		const data = fs.readFileSync('../locked.json', 'utf8');
+		fs.unlink("../locked.json");
+		var locked=JSON.parse(data);
+		for(var i in locked){
+			if(i.uuid==user.uuid){
+				delete locked[i];
+			}
+		}
+		try {
+			const data = fs.writeFileSync('../locked.json', JSON.stringify(locked));
+		} catch (err) {
+			console.error(err)
+		}
+		return;
+	}
+	static list(){
+		const data = fs.readFileSync('../locked.json', 'utf8');
+		var locked=JSON.parse(data);
+		return locked;
+	}
+	static get(user){
+		const data = fs.readFileSync('../locked.json', 'utf8');
+		fs.unlink("../locked.json");
+		var locked=JSON.parse(data);
+		for(var i in locked){
+			if(i.uuid==user.uuid){
+				return true;
+			}
+		}
+		return false;
+	}
+}
