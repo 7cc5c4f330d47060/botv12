d95503fcb1f5ddea626661dd522de675d54b6002
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2020-09-28 18:43:46 +0000

    index.js updated on 2020-09-28T18:43:46Z

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ var amount = function(dirPath,filter){
   })
   return files.length;
 }
+var pri = setInterval(function(){global.cwc("Say |help page <PAGE> for a list of commands on a page and say |help usage <COMMAND> for more detail on a command.")},300000)
 const rev = amount("nppBackup","index.js")+amount("commands/nppBackup","Command")+amount("bot_helper_scripts/nppBackup","")
 console.log("Revision "+rev)
 setTerminalTitle("Revision "+rev)
