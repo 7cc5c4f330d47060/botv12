6028af4060c83b036a0c4709faba1ad08f24c370
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2020-09-28 18:44:48 +0000

    index.js updated on 2020-09-28T18:44:48Z

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ var amount = function(dirPath,filter){
   return files.length;
 }
 var pri = setInterval(function(){global.cwc("Say |help page <PAGE> for a list of commands on a page and say |help usage <COMMAND> for more detail on a command.")},300000)
+global.cwc("Say |help page <PAGE> for a list of commands on a page and say |help usage <COMMAND> for more detail on a command.")
 const rev = amount("nppBackup","index.js")+amount("commands/nppBackup","Command")+amount("bot_helper_scripts/nppBackup","")
 console.log("Revision "+rev)
 setTerminalTitle("Revision "+rev)
