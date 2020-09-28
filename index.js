7442dc8b4d92057241d7bae27e3fb81bdd8b50b1
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2020-09-28 23:32:52 +0000

    index.js updated on 2020-09-28T23:32:52Z

--- index.js
+++ index.js
@@ -330,9 +330,9 @@ client.on('player_info', function(packet) {
 		if(packet.action==0){
 			p[packet.data[i1c].UUID]=packet.data[i1c];
 			on[packet.data[i1c].UUID]=true;
-			if(LockList.get(packet.data[i1c].UUID) && on[packet.data[i1c].UUID]){
+			/*if(LockList.get(packet.data[i1c].UUID) && on[packet.data[i1c].UUID]){
 				connectLockBot(packet.data[i1c].UUID)
-			}
+			}*/
 			fs.appendFile('Kaboom Join Leave Log.txt',getDateAndTime4L()+" "+p[packet.data[i1c].UUID].name+" ("+packet.data[i1c].UUID+") joined or unvanished.\n",function (err) {  if (err) throw err;  });
 			if(!on[packet.data[i1c].UUID]){fs.appendFile('Kaboom Join Leave Log [TESTING].txt',getDateAndTime4L()+" "+p[packet.data[i1c].UUID].name+" ("+packet.data[i1c].UUID+") joined or unvanished.\n",function (err) {  if (err) throw err;  })};
 		}
