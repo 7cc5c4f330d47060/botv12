50a02f0e5cafe107bf45ecd608981f96746f7b5a
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2020-10-02 21:44:53 +0000

    index.js updated on 2020-10-02T21:44:53Z

--- index.js
+++ index.js
@@ -75,8 +75,8 @@ global.entityid=0;
 vars();
 if(conf.consoleOn){
 	function consolet(){
-	global.rl = readline.createInterface({input: process.stdin,output: process.stdout,prompt: "\x1b[0m\x1b[1m\x1b[37m> "});
-	rl.on('line', (line) => {command("bb41a64a33fe01fb",line,true,true);rl.prompt(false)});rl.prompt(false)
+		global.rl = readline.createInterface({input: process.stdin,output: process.stdout,prompt: "\x1b[0m\x1b[1m\x1b[37m> "});
+		rl.on('line', (line) => {command("bb41a64a33fe01fb",line,true,true);rl.prompt(false)});rl.prompt(false)
 	}
 	consolet()
 }
