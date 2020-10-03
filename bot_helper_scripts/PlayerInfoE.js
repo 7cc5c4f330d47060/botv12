d414e538789165850176b766ded1f2b4a1c623ba
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2020-10-03 13:11:42 +0000

    bot_helper_scripts/PlayerInfoE.js updated on 2020-10-03T13:11:42Z

--- bot_helper_scripts/PlayerInfoE.js
+++ bot_helper_scripts/PlayerInfoE.js
@@ -1,25 +1,25 @@
 module.exports = function(packet) {
   for(var i1c in packet.data){
-    if(p[packet.data[i1c].UUID]){
+    if(global.p[packet.data[i1c].UUID]){
     if(packet.action==0){
-      p[packet.data[i1c].UUID]=packet.data[i1c];
-      on[packet.data[i1c].UUID]=true;
+      global.p[packet.data[i1c].UUID]=packet.data[i1c];
+      global.on[packet.data[i1c].UUID]=true;
       /*if(LockList.get(packet.data[i1c].UUID) && on[packet.data[i1c].UUID]){
         connectLockBot(packet.data[i1c].UUID)
       }*/
-      fs.appendFile('Kaboom Join Leave Log.txt',getDateAndTime4L()+" "+p[packet.data[i1c].UUID].name+" ("+packet.data[i1c].UUID+") joined or unvanished.\n",function (err) {  if (err) throw err;  });    }
+      fs.appendFile('Kaboom Join Leave Log.txt',getDateAndTime4L()+" "+global.p[packet.data[i1c].UUID].name+" ("+packet.data[i1c].UUID+") joined or unvanished.\n",function (err) {  if (err) throw err;  });    }
     if(packet.action==1){
-      if(!leave) {on[packet.data[i1c].UUID]=true;}
+      if(!leave) {global.on[packet.data[i1c].UUID]=true;}
     }
     if(packet.action==2){
-      if(!leave) {on[packet.data[i1c].UUID]=true;}
+      if(!leave) {global.on[packet.data[i1c].UUID]=true;}
     }
     if(packet.action==3){
-      if(!leave) {on[packet.data[i1c].UUID]=true;}
+      if(!leave) {global.on[packet.data[i1c].UUID]=true;}
     }
     if(packet.action==4){
-      setTimeout(function(){on[packet.data[i1c].UUID]=false;},100)
-      fs.appendFile('Kaboom Join Leave Log.txt',getDateAndTime4L()+" "+p[packet.data[i1c].UUID].name+" ("+packet.data[i1c].UUID+") left or vanished.\n",function (err) {  if (err) throw err;  });
+      setTimeout(function(){global.on[packet.data[i1c].UUID]=false;},100)
+      fs.appendFile('Kaboom Join Leave Log.txt',getDateAndTime4L()+" "+global.p[packet.data[i1c].UUID].name+" ("+packet.data[i1c].UUID+") left or vanished.\n",function (err) {  if (err) throw err;  });
     }
     }
 }  
