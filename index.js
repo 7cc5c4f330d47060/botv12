4ee96d70e2c29fcd29a93b8b46c187e9214e8203
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2020-10-04 19:08:26 +0000

    index.js updated on 2020-10-04T19:08:26Z

--- index.js
+++ index.js
@@ -223,7 +223,7 @@ client.on('position', function(packet) {
 		global.position.yaw+=0.01;
 		global.position.onGround=false;
 		global.client.write("flying",{onGround:false})
-		global.client.write("position",global.position)
+		global.client.write("position_look",global.position)
 	},1000)},800)
 client.on('chat', function(packet) {
   if(!global.destroyed){
