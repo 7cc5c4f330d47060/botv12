0fab69c27f9a965798ec35ff3bc44895996c09f7
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2020-10-08 11:01:41 +0000

    index.js updated on 2020-10-08T11:01:41Z

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ console.log("Loading...");
 'use strict';
 global.fs = require('fs');
 var mc = require('minecraft-protocol');
+const admin = require('is-elevated');
+if(await isElevated()){
+	console.log("The script detected usage as a user with elevated privileges. Using it with elevation can cause damage to the system. We have prevented you from using it as admin to protect your system.")
+}
 global.commandQueue=[];
 global.chatQueue=[];
 global.chatLogQueue=[];
