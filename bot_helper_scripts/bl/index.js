3bb4668e29e68b50b3dcb8cf34ed6043df97c7d8
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2020-10-10 12:13:30 +0000

    bot_helper_scripts/bl/index.js updated on 2020-10-10T12:13:30Z

--- bot_helper_scripts/bl/index.js
+++ bot_helper_scripts/bl/index.js
@@ -21,7 +21,8 @@ const tth=function(T,go){
 			if(T.color=="light_purple"){areturn+="\x1b[0m\x1b[1m\x1b[35m";			breturn+="\u00a7d";pc=T.color} else
 			if(T.color=="yellow")		{areturn+="\x1b[0m\x1b[1m\x1b[33m";			breturn+="\u00a7e";pc=T.color} else
 			if(T.color=="white")		{areturn+="\x1b[0m\x1b[1m\x1b[37m\u00a7f";			breturn+="\u00a7f";pc=T.color} else
-			if(T.color=="reset")		{areturn+="\x1b[0m\x1b[1m\x1b[37m";			breturn+="\u00a7r";pc=T.color}
+			if(T.color=="reset")		{areturn+="\x1b[0m\x1b[1m\x1b[37m";			breturn+="\u00a7r";pc=T.color} else
+			{areturn+="\x1b[0m\x1b[1m\x1b[37m";}
 			if(true==(T.bold)){areturn+="\u00a7l";breturn+="\u00a7l"};
 			if(true==(T.italic)){areturn+="\u00a7o";breturn+="\u00a7o"};
 			if(true==(T.underlined)){areturn+="\u00a7n";breturn+="\u00a7n"};
@@ -67,8 +68,8 @@ const tth=function(T,go){
 			if(T.color=="reset")		{coloraa="\x1b[0m\x1b[1m\x1b[37m\u00a7r";	colorab+="\u00a7r"} else if(!T.color && !go){coloraa="\x1b[0m\x1b[1m\x1b[37m";}
 			/**/
 			var thing=exports.tth(T.with[iz],true)
-				x=x.replace("%s",thing[0]+coloraa)
-				y=y.replace("%s",thing[1]+colorab)
+				x=x.replace("%s",coloraa+thing[0]+coloraa)
+				y=y.replace("%s",colorab+thing[1]+colorab)
 				z=z.replace("%s",thing[2])
 			}
 		}
