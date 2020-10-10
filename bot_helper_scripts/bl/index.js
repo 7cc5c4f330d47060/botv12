fcc9bc306cfc690661bef54666d66cf4dcc9dc68
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2020-10-10 20:03:52 +0000

    bot_helper_scripts/bl/index.js updated on 2020-10-10T20:03:52Z

--- bot_helper_scripts/bl/index.js
+++ bot_helper_scripts/bl/index.js
@@ -22,9 +22,8 @@ const tth=function(T,go){
 			if(T.color=="light_purple"){areturn+="\x1b[0m\x1b[1m\x1b[35m";			breturn+="\u00a7d";pc=T.color} else
 			if(T.color=="yellow")		{areturn+="\x1b[0m\x1b[1m\x1b[33m";			breturn+="\u00a7e";pc=T.color} else
 			if(T.color=="white")		{areturn+="\x1b[0m\x1b[1m\x1b[37m\u00a7f";			breturn+="\u00a7f";pc=T.color} else
-			if(T.color=="reset")		{areturn+="\x1b[0m\x1b[1m\x1b[37m";			breturn+="\u00a7r";pc=T.color} else if(T.color.startsWith("#"))		{areturn+=T.color; breturn+=T.color;} 
-		}else if (!T.color && !go)
-			{areturn+="\x1b[0m\x1b[1m\x1b[37m";} else
+			if(T.color=="reset")		{areturn+="\x1b[0m\x1b[1m\x1b[37m";			breturn+="\u00a7r";pc=T.color} else 
+		}else {areturn+="\x1b[0m\x1b[1m\x1b[37m";} else
 			
 			if(true==(T.bold)){areturn+="\u00a7l";breturn+="\u00a7l"};
 			if(true==(T.italic)){areturn+="\u00a7o";breturn+="\u00a7o"};
