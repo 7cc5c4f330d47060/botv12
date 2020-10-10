3412b0be5184136d3caeafe6b833e793ba3195ef
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2020-10-10 23:39:32 +0000

    bot_helper_scripts/bl/index.js updated on 2020-10-10T23:39:32Z

--- bot_helper_scripts/bl/index.js
+++ bot_helper_scripts/bl/index.js
@@ -21,8 +21,7 @@ const tth=function(T,go){
 			if(T.color=="light_purple"){areturn+="\x1b[0m\x1b[1m\x1b[35m";			breturn+="\u00a7d";pc=T.color} else
 			if(T.color=="yellow")		{areturn+="\x1b[0m\x1b[1m\x1b[33m";			breturn+="\u00a7e";pc=T.color} else
 			if(T.color=="white")		{areturn+="\x1b[0m\x1b[1m\x1b[37m\u00a7f";			breturn+="\u00a7f";pc=T.color} else
-			if(T.color=="reset")		{areturn+="\x1b[0m\x1b[1m\x1b[37m";			breturn+="\u00a7r";pc=T.color}else if (!T.color && !go)
-			{areturn+="\x1b[0m\x1b[1m\x1b[37m";} 
+			if(T.color=="reset")		{areturn+="\x1b[0m\x1b[1m\x1b[37m";			breturn+="\u00a7r";pc=T.color}
 			if(true==(T.bold)){areturn+="\u00a7l";breturn+="\u00a7l"};
 			if(true==(T.italic)){areturn+="\u00a7o";breturn+="\u00a7o"};
 			if(true==(T.underlined)){areturn+="\u00a7n";breturn+="\u00a7n"};
@@ -65,8 +64,7 @@ const tth=function(T,go){
 			if(T.color=="light_purple"){coloraa="\x1b[0m\x1b[1m\x1b[35m";			colorab+="\u00a7d"} else
 			if(T.color=="yellow")		{coloraa="\x1b[0m\x1b[1m\x1b[33m";			colorab+="\u00a7e"} else
 			if(T.color=="white")		{coloraa="\x1b[0m\x1b[1m\x1b[37m";			colorab+="\u00a7f"} else
-			if(T.color=="reset")		{coloraa="\x1b[0m\x1b[1m\x1b[37m\u00a7r";	colorab+="\u00a7r"} else
-			if(!T.color && !go){coloraa="\x1b[0m\x1b[1m\x1b[37m";}
+			if(T.color=="reset")		{coloraa="\x1b[0m\x1b[1m\x1b[37m\u00a7r";	colorab+="\u00a7r"} else if(!T.color && !go){coloraa="\x1b[0m\x1b[1m\x1b[37m";}
 			/**/
 			var thing=exports.tth(T.with[iz],true)
 				x=x.replace("%s",coloraa+thing[0]+coloraa)
@@ -77,7 +75,6 @@ const tth=function(T,go){
 		areturn+= x;
 		breturn+= y;
 		creturn+= z;
-		
 	} else if (T.text != undefined){
 		areturn+= T.text;
 		breturn+= T.text;
@@ -87,12 +84,28 @@ const tth=function(T,go){
 		breturn+= ""+T;
 		creturn+= ""+T;
 	}
-
+			if(T.color=="black")	    {areturn+="\x1b[0m\x1b[2m\x1b[30m\x1b[47m";	breturn+="\u00a70";pc=T.color} else
+			if(T.color=="dark_blue")   {areturn+="\x1b[0m\x1b[2m\x1b[34m";			breturn+="\u00a71";pc=T.color} else
+			if(T.color=="dark_green")  {areturn+="\x1b[0m\x1b[2m\x1b[32m";			breturn+="\u00a72";pc=T.color} else
+			if(T.color=="dark_aqua")   {areturn+="\x1b[0m\x1b[2m\x1b[36m";			breturn+="\u00a73";pc=T.color} else
+			if(T.color=="dark_red")    {areturn+="\x1b[0m\x1b[2m\x1b[31m";			breturn+="\u00a74";pc=T.color} else
+			if(T.color=="dark_purple") {areturn+="\x1b[0m\x1b[2m\x1b[35m";			breturn+="\u00a75";pc=T.color} else
+			if(T.color=="gold")        {areturn+="\x1b[0m\x1b[2m\x1b[33m";			breturn+="\u00a76";pc=T.color} else
+			if(T.color=="gray")        {areturn+="\x1b[0m\x1b[2m\x1b[37m";			breturn+="\u00a77";pc=T.color} else
+			if(T.color=="dark_gray")	{areturn+="\x1b[0m\x1b[1m\x1b[30m";			breturn+="\u00a78";pc=T.color} else
+			if(T.color=="blue")		{areturn+="\x1b[0m\x1b[1m\x1b[34m";				breturn+="\u00a79";pc=T.color} else
+			if(T.color=="green")		{areturn+="\x1b[0m\x1b[1m\x1b[32m";			breturn+="\u00a7a";pc=T.color} else
+			if(T.color=="aqua")		{areturn+="\x1b[0m\x1b[1m\x1b[36m";				breturn+="\u00a7b";pc=T.color} else
+			if(T.color=="red")			{areturn+="\x1b[0m\x1b[1m\x1b[31m";			breturn+="\u00a7c";pc=T.color} else
+			if(T.color=="light_purple"){areturn+="\x1b[0m\x1b[1m\x1b[35m";			breturn+="\u00a7d";pc=T.color} else
+			if(T.color=="yellow")		{areturn+="\x1b[0m\x1b[1m\x1b[33m";			breturn+="\u00a7e";pc=T.color} else
+			if(T.color=="white")		{areturn+="\x1b[0m\x1b[1m\x1b[37m";			breturn+="\u00a7f";pc=T.color} else
+			if(T.color=="reset")		{areturn+="\x1b[0m\x1b[1m\x1b[37m";			breturn+="\u00a7r";pc=T.color}
 		if (T.extra){
 			for(var iza=0; iza<=T.extra.length-1; iza++){
-				areturn+= tth(T.extra[iza])[0]
-				breturn+= tth(T.extra[iza])[1]
-				creturn+= tth(T.extra[iza])[2]
+				areturn+= tth(T.extra[iza],true)[0]
+				breturn+= tth(T.extra[iza],true)[1]
+				creturn+= tth(T.extra[iza],true)[2]
 			}
 		}
 	return [(areturn+""),(breturn+""),(creturn+"")];
