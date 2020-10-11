aaadd22b27e0f32f6ec2109de127bd19f544d7f9
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2020-10-11 18:50:45 +0000

    index.js updated on 2020-10-11T18:50:45Z

--- index.js
+++ index.js
@@ -153,9 +153,9 @@ global.confirmQueueMove = function(hash){
   }
   return 0;
 }
-global.confirmQueuePush = (command,perm)=>{
-  global.confirmQueue.push({cmd:command,perm:perm})
-  global.cwc(global.csl[0]+"Are you sure you want to run \""+global.csl[1]+"|"+command.slice(0,75)+global.csl[0]+"\"? Type \""+global.csl[1]+"|confirm <CONSOLE-CODE>"+global.csl[0]+"\" to confirm.")
+global.confirmQueuePush=(c,p)=>{
+  global.confirmQueue.push({cmd:c,perm:p})
+  global.cwc(global.csl[0]+"Are you sure you want to run \""+global.csl[1]+"|"+c.slice(0,75)+global.csl[0]+"\"? Type \""+global.csl[1]+"|confirm <CONSOLE-CODE>"+global.csl[0]+"\" to confirm.")
 }
 
 global.getDateAndTime4L=require("./bot_helper_scripts/date.js")
