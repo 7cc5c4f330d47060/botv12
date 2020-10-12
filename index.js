47ee46664ee0cb9d63488dd6a91ef73ce3014e3f
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2020-10-12 00:00:04 +0000

    index.js updated on 2020-10-12T00:00:04Z

--- index.js
+++ index.js
@@ -54,66 +54,6 @@ global.connect=function(){
     version: conf.version, 
     username: "\u00a7"+Math.floor(Math.random()*16).toString(16)+"\u00a7\u00a7"+["\u0000","\u0001","\u0002","\u0003","\u0004","\u0005","\u0006","\u0007"][Math.floor(Math.random()*8)]+["\u0008","\u0009","\u007f","\u000b","\u000c","\u000d","\u000e","\u000f"][Math.floor(Math.random()*8)]+["\u0010","\u0011","\u0012","\u0013","\u0014","\u0015","\u0016","\u0017"][Math.floor(Math.random()*8)]+["\u0018","\u0019","\u001a","\u001b","\u001c","\u001d","\u001e","\u001f"][Math.floor(Math.random()*8)]+"   ",
   });
-  const states = mc.states
-  global.mcData = require('minecraft-data')(conf.version)
-  global.loginPacket = mcData.loginPacket
-const srv = mc.createServer({'online-mode': false,port: 40000,keepAlive: false,'max-players': 2})
-srv.on('login', function (cliet) {
-setTimeout(function(){cliet.write('login', {
-    entityId: entityid,
-    gameMode: 1,
-    dimension: 1,
-	difficulty: 1,
-	maxPlayers: 9,
-    levelType: "a",
-    reducedDebugInfo: false
-  })
-    cliet.write('position', {
-    x: global.position.x,
-    y: global.position.y,
-    z: global.position.z,
-    yaw: 0,
-    pitch: 0,
-    flags: 0x00
-})},500)
-  let endedClient = false
-  let endedTargetClient = false
-  cliet.on('end', function () {
-    endedClient = true
-  })
-  cliet.on('error', function (err) {
-    endedClient = true
-  })
-  allow=0;
-  setTimeout(function(){allow=1},1000)
-  cliet.on('packet', function (data, meta) {
-    if (client.state === states.PLAY && meta.state === states.PLAY && allow) {
-      if (!endedTargetClient) { client.write(meta.name, data) }
-    }
-  })
-  client.on('packet', function (data, meta) {
-    if (meta.state === states.PLAY && cliet.state === states.PLAY && allow) {
-      if (!endedClient) {
-        cliet.write(meta.name, data)
-        if (meta.name === 'set_compression') {
-          cliet.compressionThreshold = data.threshold
-        } 
-      }
-    }
-  })
-  client.on('kick_disconnect', function (gt) {
-    endedTargetClient = true
-    if (!endedClient) { cliet.end(gt.reason) }
-  })
-  client.on('end', function () {
-    endedTargetClient = true
-    if (!endedClient) { cliet.end('End') }
-  })
-  client.on('error', function (err) {
-    endedTargetClient = true
-    if (!endedClient) { cliet.end('Error') }
-  })
-})
 }
 global.setup2=function(){
 global.consoleOnly = conf.consoleOnly; global.fileLogger = conf.fileLoggerOn; global.consoleLogger = conf.consoleLoggerOn; global.pll = conf.permLevelList; global.cspyMode=conf.cspyOn; global.csl=conf.cs; global.prefix=conf.prefix;
