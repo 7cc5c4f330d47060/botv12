b82a92165e09424d1de203b0584743438073bdb6
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2020-10-18 16:33:20 +0000

    bot_helper_scripts/LSetup2.js updated on 2020-10-18T16:33:20Z

--- bot_helper_scripts/LSetup2.js
+++ bot_helper_scripts/LSetup2.js
@@ -10,7 +10,7 @@ global.cwc=T=>{global.chatQueue.push(T)}
 global.pri = setInterval(()=>{global.cwc(conf.chat.split("%prefix%").join(global.prefix))},conf.chatInterval);global.cwc(conf.chat.split("%prefix%").join(global.prefix))
 global.packetc=conf.packetSet;
 global.pki = setInterval(()=>{packetc--;if(packetc<=0){process.exit(0)} else if(packetc<=conf.packetCountdown){console.log(packetc)}},1000)
-for(var i1b in global.commands){global.cmdid.push({name:i1b,h:commands[i1b].h,usage:commands[i1b].u})}
+for(var i1b in global.commands){global.cmdid.push({name:i1b,h:Commands.commands[i1b].h,usage:Commands.commands[i1b].u})}
 if(conf.revision){global.cwc("Version "+global.rev)}
 global.cwc("/cspy "+["off","on"][+global.cspyMode])
 for(var i in conf.run){global.cwc(conf.run[i])}
@@ -19,9 +19,9 @@ global.command=(n,d,b1a,C)=>{
 var c=d.toLowerCase();
 if(process.memoryUsage().heapUsed>=650000000 && c.split(" ")[0]!="eval" && c.split(" ")[0]!="logger" && c.split(" ")[0]!="clearcmdq" && c.split(" ")[0]!="restart" ){if(C){console.log("Not enough memory available to run command")}else{global.cwc("Not enough memory available to run command")};return}
   if(!global.consoleOnly || C || getPerm(n)==11){
-  if(commands[c.split(" ")[0]]){if(commands[c.split(" ")[0]].console && !(C || getPerm(n)==11)){cwc(`/bc &r${global.prefix}${c.split(" ")[0]} may only be run from console.`)}else{
-    if(getPerm(n)>=commands[c.split(" ")[0]].perm || C || getPerm(n)==11){
-      if(!commands[c.split(" ")[0]].confirm || b1a || C || getPerm(n)==11){
+  if(Commands.commands[c.split(" ")[0]]){if(commands[c.split(" ")[0]].console && !(C || getPerm(n)==11)){cwc(`/bc &r${global.prefix}${c.split(" ")[0]} may only be run from console.`)}else{
+    if(getPerm(n)>=Commands.commands[c.split(" ")[0]].perm || C || getPerm(n)==11){
+      if(!Commands.commands[c.split(" ")[0]].confirm || b1a || C || getPerm(n)==11){
         Commands.commands[c.split(" ")[0]].command(d,n,C || getPerm(n)==11);
         return 0;
       } else {
