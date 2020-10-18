66c7eef6f9c622f45eecfa29b842a2bc87bcc68f
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2020-10-18 15:01:41 +0000

    bot_helper_scripts/LInit.js updated on 2020-10-18T15:01:41Z

---
+++ bot_helper_scripts/LInit.js
@@ -0,0 +1,22 @@
+()=>{
+  global.rh=require(bhs+'Hash.js');
+  global.adminCode = 0; global.adminCode=rh();
+  console.log(global.adminCode)
+  global.moduleDetector=global.perms = require(bhs+'ModuleDetector.js'); moduleDetector("mineflayer",true,()=>{});
+  global.amount = (dirPath,filter)=>{
+    var files2 = fs.readdirSync(dirPath)
+    var files=[];
+    files2.forEach((f)=>{
+      if(f.startsWith(filter)){files.push(f)}
+    })
+    return files.length;
+  }
+  global.realRev = amount("nppBackup","index.js")+amount("commands/nppBackup","Command")+amount(bhs+"nppBackup","")
+  console.log("Revision "+realRev)
+  global.type=["Debug","Normal"][+conf.isNormal]
+  global.title = (title)=>{process.stdout.write(String.fromCharCode(27)+"]0;"+title+String.fromCharCode(7));}
+  global.perms = require('./admins.json');
+  global.rev2 = conf.rev;
+  global.rev = rev2 + ` [${type}]`;
+  if(conf.revision){console.log("Version "+global.rev);title("NCB Version "+global.rev)}
+}
