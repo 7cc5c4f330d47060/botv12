d395b3edc0a1057e4e839028c01a89a252fd80a6
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2020-10-18 15:08:20 +0000

    bot_helper_scripts/LSetup.js updated on 2020-10-18T15:08:20Z

---
+++ bot_helper_scripts/LSetup.js
@@ -0,0 +1,19 @@
+module.exports=()=>{
+  setTimeout(()=>{process.exit(0)},conf.restartTimer)
+  global.commands={};global.p={};
+  setInterval(()=>{global.gc();}, 5000);
+  require('./commands/Commands.js')();
+  global.lang=require(bhs+'bl/index.js');
+  global.rq=require;
+  global.readline = require("readline");
+  global.c2 = new require("net").Socket().connect(41050, 'localhost',()=>{});
+
+  setTimeout(()=>{global.cl=setInterval(chatLogQueueMove,conf.chatLogQueueSpeed)},5000);
+  setTimeout(()=>{chatQueueMove()},4000);
+  setTimeout(()=>{global.cd=setInterval(cmdQueueMove,conf.commandQueueSpeed)},1000);
+
+  global.chatQueueR=(t)=>{
+    global.chatQueueSpeed=t
+    cwc("Chat speed set to "+t+"ms.")
+  }
+}
