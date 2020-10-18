4bed5e3fb76e8de5bda5a0188c3e7b51fe5a1bf7
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2020-10-18 15:09:23 +0000

    index.js updated on 2020-10-18T15:09:23Z

--- index.js
+++ index.js
@@ -13,16 +13,6 @@ global.setup=require(bs+"LSetup.js");
 global.connect=require(bs+"LConnect.js");
 global.setup2=require(bs+"LSetup2.js");
 global.pt=a=>{var b=a;delete b.flags;delete b.teleportId;b.onGround=true;return b}
-global.events=function(){
-global.playerInfo=require(bhs+"PlayerInfoE.js"); global.ChatE=require(bhs+"ChatE.js");
-client.on('player_info', global.playerInfo)
-client.on('kick_disconnect', p=>{console.log(lang.tth(JSON.parse(p.reason.split("\n").join("\\n"))));setTimeout(function(){process.exit(0)},2000)})
-client.on('tab_complete', p=>{global.cwc(global.csl[0]+"Results: "+global.csl[1]+p.matches.length);for(var i5a in p.matches){global.cwc(global.csl[1]+p.matches[i5a].match)}})
-client.on('login', p=>{if(p.entityId){global.entityid=p.entityId}})
-client.on('position', p=>{global.position=p;/*if(global.pa){global.pa=true;client.write("teleport_confirm",{teleportId:(p.teleportId?p.teleportId:0)});client.write("position_look",global.pt(position))}*/})
-client.on('entity_status', p=>{if(p.entityId==global.entityid && p.entityStatus == 24){global.cwc("/op @s[type=player]")}})
-client.on('chat', ChatE);
-client.on('packet', (data, meta)=>{packetc=conf.packetSet;})
-}
+global.events=require(bs+"LEvens.js");
 
 require("./run.js")()
