912e31597078a5d21daf86e6166be531bea44635
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2020-10-20 14:22:16 +0000

    c.js updated on 2020-10-20T14:22:16Z

---
+++ c.js
@@ -0,0 +1,24 @@
+var net = require('net');
+var server = net.createServer();
+server.on('connection', h);
+server.listen(41050);
+function h(c) {    
+  c.on('data', a);  
+  c.on('close', tyy);  
+  function a(d6) {  
+	if(d6[0]==1){
+	var $$ = d6.toString('utf8').slice(1);
+    console.log($$);
+	delete d6;
+	} else if (d6[0]==2){
+		console.clear();
+	} else if (d6[0]==3){
+		console.clear();
+		process.exit(0);
+	}
+
+  }
+  function tyy(e){
+	  
+  }
+}
