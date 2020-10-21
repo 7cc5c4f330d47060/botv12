ef267703a6b3c2bcc24c0d45fb31904cbee597af
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2020-10-21 19:38:34 +0000

    bot_helper_scripts/LEvents.js updated on 2020-10-21T19:38:34Z

--- bot_helper_scripts/LEvents.js
+++ bot_helper_scripts/LEvents.js
@@ -9,10 +9,31 @@ p=>{
 	}
 )
 client.on('tab_complete', p=>{
-	global.cwc(global.csl[0]+"Results: "+global.csl[1]+p.matches.length);
-	for(var i5a = 0; i5a <= Math.min(p.matches[i5a].match,90); i5a+=3){
-		global.cwc(global.csl[1]+p.matches[i5a*3].match+" "+global.csl[1]+p.matches[(i5a*3)+1].match+" "+global.csl[1]+p.matches[i5a*3].match)
-	}
+		global.cwc(global.csl[0]+"Results: "+global.csl[1]+p.matches.length);
+	if(p.transactionId==515514){
+		for(var i5a in p.matches){
+			global.cwc("/team remove "+p.matches[i5a].match)
+		}
+	} else if(p.transactionId==515504){
+		for(var i5a in p.matches){
+			global.cwc("/bossbar remove "+p.matches[i5a].match)
+		}
+	} else if(p.transactionId==424414){
+		for(var i5a in p.matches){
+			global.cwc("/scoreboard objectives remove "+p.matches[i5a].match)
+		}
+	} else if(p.transactionId==423314){
+		for(var i5a in p.matches){
+			global.cwc("/pardon "+p.matches[i5a].match)
+		}
+	} else if(p.transactionId==422214){
+		for(var i5a in p.matches){
+			global.cwc("/pardon-ip "+p.matches[i5a].match)
+		}
+	} else if(p.transactionId==399214){
+		for(var i5a in p.matches){
+			global.cwc("/deop "+p.matches[i5a].match)
+		}
 	}
 )
 client.on('login', p=>{
