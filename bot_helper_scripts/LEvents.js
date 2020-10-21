7d868c87ac8eba86860bf080acd9773df8fb207a
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2020-10-21 13:19:33 +0000

    bot_helper_scripts/LEvents.js updated on 2020-10-21T13:19:33Z

---
+++ bot_helper_scripts/LEvents.js
@@ -0,0 +1,11 @@
+module.exports=()=>{
+global.playerInfo=require(bhs+"PlayerInfoE.js"); global.ChatE=require(bhs+"ChatE.js");
+client.on('player_info', global.playerInfo)
+client.on('kick_disconnect', p=>{console.log(lang.tth(JSON.parse(p.reason.split("\n").join("\\n"))));setTimeout(function(){process.exit(0)},2000)})
+client.on('tab_complete', p=>{global.cwc(global.csl[0]+"Results: "+global.csl[1]+p.matches.length);for(var i5a in p.matches){global.cwc(global.csl[1]+p.matches[i5a].match)}})
+client.on('login', p=>{if(p.entityId){global.entityid=p.entityId}})
+client.on('position', p=>{global.position=p;/*if(global.pa){global.pa=true;client.write("teleport_confirm",{teleportId:(p.teleportId?p.teleportId:0)});client.write("position_look",global.pt(position))}*/})
+client.on('entity_status', p=>{if(p.entityId==global.entityid && p.entityStatus == 24){global.cwc("/op @s[type=player]")}})
+client.on('chat', ChatE);
+client.on('packet', (data, meta)=>{packetc=conf.packetSet;})
+}
