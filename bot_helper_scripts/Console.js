4b298fa818c933ee3dd1bcd82aef44b5200ca4d5
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2020-10-25 18:06:25 +0000

    bot_helper_scripts/Console.js updated on 2020-10-25T18:06:25Z

--- bot_helper_scripts/Console.js
+++ bot_helper_scripts/Console.js
@@ -1,1 +1,14 @@
-{\rtf1}
+module.exports=function(){
+  global.rl = readline.createInterface(
+    {
+	  input: process.stdin,
+	  output: process.stdout,
+	  prompt: "\x1b[0m\x1b[1m\x1b[37m> "
+	}
+  );
+  rl.on('line', (line) => {
+    global.command("bb41a64a33fe01fb",line,true,true);
+    rl.prompt(false)
+  });
+  rl.prompt(false)
+}
