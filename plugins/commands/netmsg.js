2b707aca1af4a6900b27c9556748c0e232c755da
7cc5c4f330d47060 <exploding.kittens.a1b2c3@gmail.com>
2024-07-16 17:30:37 -0400

    Add netmsg command

---
+++ plugins/commands/netmsg.js
@@ -0,0 +1,39 @@
+const {bot}=require("../../index.js");
+module.exports={
+    execute: (c)=>{
+        const json={
+            translate: "[%s] %s: %s",
+            with:[
+                {
+                    translate: "%s:%s",
+                    with:[
+                        {
+                            text: c.host,
+                            color: "light_purple"
+                        },
+                        {
+                            text: c.port+"",
+                            color: "light_purple"
+                        }
+                    ],
+                    color: "gray"
+                },
+                {
+                    text: c.username,
+                    color: "light_purple"
+                },
+                {
+                    text: c.args.join(" ")
+                },
+            ],
+            color: "white"
+        }
+        for(const i in bot){
+            bot[i].tellraw("@a",JSON.stringify(json))
+        }
+    },
+    desc: "Send a message to all servers the bot is connected to", // Command description
+    usage: ' <message>', // Command usage
+    hidden: true, // To show the command on the help command list, remove this line
+    consoleIndex: true // When run from console, the second argument will be a bot ID
+}
