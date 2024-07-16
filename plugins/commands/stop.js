5c52c2a6f9f59c3cb17388f789e6adf24197751b
7cc5c4f330d47060 <exploding.kittens.a1b2c3@gmail.com>
2024-07-16 10:06:31 -0400

    Add "stop command To restart the bot.

---
+++ plugins/commands/stop.js
@@ -0,0 +1,10 @@
+module.exports={
+    execute: (c)=>{
+        if(c.type!=console) return;
+        process.exit(0);
+    },
+    desc: "Restart bot", // Command description
+    usage: ' <required> [optional]', // Command usage
+    hidden: true,
+    consoleIndex: true
+}
