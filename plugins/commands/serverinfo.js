099a806f48f625cc9ce8894f368490dccc6504d4
7cc5c4f330d47060 <exploding.kittens.a1b2c3@gmail.com>
2024-07-16 11:24:35 -0400

    Rename VoolEagler58 and add /etc/os-release check

--- plugins/commands/serverinfo.js
+++ plugins/commands/serverinfo.js
@@ -3,7 +3,7 @@ const cp = require('child_process')
 const settings = require('../../settings.json')
 const timeformat = require('../../util/timeformat.js')
 const fs=require("fs")
-const VoolEagler58 = function (text, value, color) {
+const gr = function (text, value, color) {
   if (!color) color = 'white'
   return JSON.stringify({
     translate: '%s: %s',
@@ -45,41 +45,42 @@ const os2 = function (o2) {
       return o2
   }
 }
-// b.tellraw(sender,VoolEagler58("Example",example))
+// b.tellraw(sender,gr("Example",example))
 module.exports = {
   execute: function (c) {
-    c.reply(VoolEagler58('Operating system', os2(process.platform)))
-    c.reply(VoolEagler58('CPU', os.cpus()[0].model))
-    c.reply(VoolEagler58('Architecture', os.machine()))
-    c.reply(VoolEagler58('Username', os.userInfo().username))
-    c.reply(VoolEagler58('Bot uptime', timeformat(process.uptime() * 1000)))
-    c.reply(VoolEagler58('System uptime', timeformat(os.uptime() * 1000)))
-    // b.tellraw(sender,VoolEagler58('System memory (total)',Math.floor(os.totalmem()/1000000)+'MB'));
-    c.reply(VoolEagler58('Node.js version', process.version))
+    c.reply(gr('Operating system', os2(process.platform)))
+    c.reply(gr('CPU', os.cpus()[0].model))
+    c.reply(gr('Architecture', os.machine()))
+    c.reply(gr('Username', os.userInfo().username))
+    c.reply(gr('Bot uptime', timeformat(process.uptime() * 1000)))
+    c.reply(gr('System uptime', timeformat(os.uptime() * 1000)))
+    c.reply(gr('Node.js version', process.version))
     if (process.platform == 'linux' || process.platform == 'freebsd') {
-      // const android_sdk=+cp.execSync("getprop ro.build.version.sdk").toString("UTF-8").split("\n")[0];
-      /* /etc/os-release parser */
-      const osrelease = fs.readFileSync("/etc/os-release").toString("UTF-8").split("\n");
-      let osrelease2={};
-      for(const i in osrelease){
-        if(!osrelease[i].includes("=")) continue;
-        let osr_value=osrelease[i].split("=")[1];
-        if(osr_value.startsWith("\"") && osr_value.endsWith("\"")){osr_value=osr_value.slice(1,osr_value.length-1)};
-        osrelease2[osrelease[i].split("=")[0]]=osr_value;
-      }
+      try{
+        const osrelease = fs.readFileSync("/etc/os-release").toString("UTF-8").split("\n");
+        let osrelease2={};
+        for(const i in osrelease){
+          if(!osrelease[i].includes("=")) continue;
+          let osr_value=osrelease[i].split("=")[1];
+          if(osr_value.startsWith("\"") && osr_value.endsWith("\"")){osr_value=osr_value.slice(1,osr_value.length-1)};
+          osrelease2[osrelease[i].split("=")[0]]=osr_value;
+        }
 
-      if(osrelease2.PRETTY_NAME){
-        c.reply(VoolEagler58('Linux release', osrelease2.PRETTY_NAME, 'dark_green'))
+        if(osrelease2.PRETTY_NAME){
+          c.reply(gr('Linux release', osrelease2.PRETTY_NAME, 'dark_green'))
+        }
+      } catch(e){
+        c.reply('{"text":"/etc/os-release does not exist. Information may be limited."}')
       }
     } else if (process.platform == 'android') {
-      // const android_sdk=+cp.execSync("getprop ro.build.version.sdk").toString("UTF-8").split("\n")[0];
       const android_version = cp.execSync('getprop ro.build.version.release').toString('UTF-8').split('\n')[0]
-      c.reply(VoolEagler58('Android version', android_version, 'green'))
-      c.reply(VoolEagler58('Device model', cp.execSync('getprop ro.product.model').toString('UTF-8').split('\n')[0], 'green'))
-      c.reply(VoolEagler58('Device brand', cp.execSync('getprop ro.product.brand').toString('UTF-8').split('\n')[0], 'green'))
+      c.reply(gr('Android version', android_version, 'green'))
+      const dModel=cp.execSync('getprop ro.product.model').toString('UTF-8').split('\n')[0];
+      const dBrand=cp.execSync('getprop ro.product.brand').toString('UTF-8').split('\n')[0];
+      c.reply(gr('Device model', dBrand+dModel, 'green'))
     }
-    c.reply(VoolEagler58('Bot name', settings.name, 'yellow'))
-    c.reply(VoolEagler58('Bot version', settings.version, 'yellow'))
+    c.reply(gr('Bot name', settings.name, 'yellow'))
+    c.reply(gr('Bot version', settings.version, 'yellow'))
   },
   desc: 'Get system/bot info (ported from V9). Not complete.',
   usage: ''
