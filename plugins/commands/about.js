70f26e6527122604bbf703cd117ba4d553e17157
7cc5c4f330d47060 <exploding.kittens.a1b2c3@gmail.com>
2024-07-16 09:56:08 -0400

    Change about command message

--- plugins/commands/about.js
+++ plugins/commands/about.js
@@ -1,14 +1,13 @@
 // This is not C source code
 //const settings = require('../settings.json')
+const settings = require("../../settings.json")
 module.exports = {
   execute: function (c) {
     c.reply('{"text":"UBot - a minecraft bot made by a \\"baby\\""}');
     c.reply('{"text":""}');
+    c.reply('{"text":"Version '+settings.version+'"}');
+    c.reply('{"text":""}');
     c.reply('{"text":"To view system information, run the command \\"serverinfo\\"."}')
-    c.reply('{"text":""}');
-    c.reply('{"text":"This is a new version so most features from version 9 and older have not been re-implemented."}')
-    c.reply('{"text":""}');
-    c.reply('{"text":"The first version of UBot was made in August 2020."}')
   },
   desc: 'About the bot',
   usage: ''
