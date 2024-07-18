bb4b9eba03688d27fce8e979e660da67b522ffac
7cc5c4f330d47060 <exploding.kittens.a1b2c3@gmail.com>
2024-07-18 16:55:23 -0400

    Add eval command

---
+++ plugins/commands/eval.js
@@ -0,0 +1,17 @@
+const textformat=require("../../util/textformat.js")
+const index=require("../../index.js") //Not used in the code, but may be used by users of the command
+module.exports={
+    execute: (c)=>{
+        if(c.type!="console"){
+            c.reply(textformat("Eval command is currently not available to players in Minecraft"))
+        };
+        try{
+            console.log(eval(c.args.join(" ")));
+        }catch(e){
+            console.error(e);
+        }
+    },
+    desc: "Run JavaScript code", // Command description
+    usage: '', // Command usage
+    hidden: true
+}
