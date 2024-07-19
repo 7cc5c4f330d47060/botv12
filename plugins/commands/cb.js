a2cd7e5a99c3572a6d414138310562cb6dcdaa57
7cc5c4f330d47060 <exploding.kittens.a1b2c3@gmail.com>
2024-07-19 16:41:04 -0400

    Unhide some commands

--- plugins/commands/cb.js
+++ plugins/commands/cb.js
@@ -4,7 +4,6 @@ module.exports={
     },
     desc: "Run a command in a command block", // Command description
     usage: ' <command>', // Command usage
-    hidden: true, // To show the command on the help command list, remove this line
     consoleIndex: true, // When run from console, the second argument will be a bot ID
     aliases: ["commandblock", "cmdblock"] // Other command names that will work the same
 }
