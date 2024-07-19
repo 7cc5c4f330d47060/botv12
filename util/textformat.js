5f01efb646857eb1c209a02372f3c4d13887d802
7cc5c4f330d47060 <exploding.kittens.a1b2c3@gmail.com>
2024-07-19 11:45:40 -0400

    Change textformat.js formatting to be similar to /say

--- util/textformat.js
+++ util/textformat.js
@@ -1,6 +1,17 @@
+const settings=require("../settings.json");
 module.exports = function (text) {
   return JSON.stringify({
-    text: text,
-    color: "light_purple"
+    translate: "[%s] %s",
+    color: "#FFAAFF",
+    with:[
+      {
+        text: settings.name,
+        color: "light_purple"
+      },
+      {
+        text: text,
+        color: "white"
+      }
+    ]
   })
 }
