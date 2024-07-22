903e7ed793f5bbc36d59f7dc0386eff9aba28d5f
7cc5c4f330d47060 <exploding.kittens.a1b2c3@gmail.com>
2024-07-22 18:26:14 -0400

    Convert high permission commands to use permission levels

--- plugins/commands/logoff.js
+++ plugins/commands/logoff.js
@@ -1,10 +1,10 @@
 module.exports={
     execute: (c)=>{
-        if(c.verify<2) return;
         c.bot._client.end();
     },
     desc: "Does nothing",
     usage: ' <required> [optional]',
     hidden: true,
-    consoleIndex: true
+    consoleIndex: true,
+    level: 2
 }
