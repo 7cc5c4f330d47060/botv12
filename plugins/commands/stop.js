da32a96b05d8707e701cffcd74eafe7744c35148
7cc5c4f330d47060 <exploding.kittens.a1b2c3@gmail.com>
2024-07-22 18:26:14 -0400

    Convert high permission commands to use permission levels

--- plugins/commands/stop.js
+++ plugins/commands/stop.js
@@ -1,10 +1,10 @@
 module.exports={
     execute: (c)=>{
-        if(c.verify<2) return;
         process.exit(0);
     },
     desc: "Restart bot",
     usage: ' <required> [optional]',
     hidden: true,
-    aliases: ["restart", "exit"]
+    aliases: ["restart", "exit"],
+    level: 2
 }
