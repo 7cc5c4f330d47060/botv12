196060f791ee6c2d4ea90ad5110b2607ccd4751d
7cc5c4f330d47060 <exploding.kittens.a1b2c3@gmail.com>
2024-07-23 17:18:55 -0400

    Add cloop command

---
+++ plugins/commands/cloop.js
@@ -0,0 +1,36 @@
+const getMessage = require('../../util/lang.js')
+module.exports={
+    execute: (c)=>{
+        const subcmd=c.args.splice(0,1)[0];
+        switch(subcmd){
+            case "add":
+                const rate=+(c.args.splice(0,1)[0]);
+                const command=c.args.join(" ");
+                if(rate<20){
+                    c.reply({text:getMessage(c.lang,"command.cloop.error.tooShort")})
+                }
+                c.bot.addCloop(command,rate)
+                c.reply({text:getMessage(c.lang,"command.cloop.success.add",[command,rate+""])})
+                break
+            case "remove":
+                const index=+c.args[0];
+                c.bot.removeCloop(c.args[0]);
+                c.reply({text:getMessage(c.lang,"command.cloop.success.remove",[index+""])})
+                break
+            case "list":
+                for(const i in c.bot.cloops){
+                    c.reply({text:getMessage(c.lang,"command.cloop.list",[i,c.bot.cloops[i].command,c.bot.cloops[i].rate+""])})
+                }
+                break
+            case "clear":
+                c.bot.clearCloops();
+                c.reply({text:getMessage(c.lang,"command.cloop.success.clear")})
+                break
+            default:
+                c.reply(`Unknown subcommand, please do ${c.prefix}help cloop`)
+        }
+    },
+    hidden: true,
+    consoleIndex: true,
+    level: 1
+}
