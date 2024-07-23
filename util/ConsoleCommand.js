1da0188303e182a7932a83b5a1e56f153782c4d6
7cc5c4f330d47060 <exploding.kittens.a1b2c3@gmail.com>
2024-07-22 21:52:18 -0400

    Fix console command bugs

--- util/ConsoleCommand.js
+++ util/ConsoleCommand.js
@@ -3,10 +3,11 @@ const index = require('../index.js')
 const { cmds } = require('../plugins/command.js');
 const parse = require("../util/chatparse.js");
 const settings = require("../settings.json");
+const lang=settings.defaultLang;
 class ConsoleCommand{
 	constructor (cmd,index2){
 		this.send = ()=>{}; //not needed for console
-		this.reply = text => process.stdout.write(parse(JSON.parse(text))[0]+'\n');
+		this.reply = text => process.stdout.write(parse(text)[0]+'\n');
 		this.uuid = 'dde5a2a6-ebdd-4bbb-8eac-f75b10c10446_console'; //hard-coded because uuid does not exist at console
 		this.username = 'Owner';
 		this.nickname = 'Console';
@@ -19,10 +20,18 @@ class ConsoleCommand{
 					//if(cmds[i].hidden) continue;
 					helpCmds.push(i)
 				}
-				console.log("Commands: "+helpCmds.join(" "))
+				console.log(getMessage(lang,"command.help.cmdList",[helpCmds.join(" ")]))
 			},
 			printCmdHelp:(uuid,cmd)=>{
-				console.log(cmd+cmds[cmd].usage+" - "+cmds[cmd].desc);
+				let usage=getMessage(lang,`command.${cmd}.usage`);
+				let desc=getMessage(lang,`command.${cmd}.desc`);
+				if(cmds[cmd].usage){
+					usage=cmds[cmd].usage;
+				}
+				if(cmds[cmd].desc){
+					desc=cmds[cmd].desc;
+				}
+				console.log(getMessage(lang,"command.help.commandInfo",[cmd,usage,desc]));
 			}
 		}; //bot does not exist at console
 		this.type = 'console';
