5834b0ae565eb6839343b51c48bee5c7c2753f74
7cc5c4f330d47060 <exploding.kittens.a1b2c3@gmail.com>
2024-07-24 02:55:10 -0400

    Make about and serverinfo show Git commit

--- plugins/commands/about.js
+++ plugins/commands/about.js
@@ -5,7 +5,17 @@ module.exports = {
   execute: function (c) {
     c.reply({text:getMessage(c.lang,"command.about.author",[settings.name])});
     c.reply({text:""});
-    c.reply({text:getMessage(c.lang,"command.about.version",[version.bot])});
+    let botVersion=version.bot;
+    let gitCommit;
+    try {
+      gitCommit = cp.execSync('git rev-parse --short HEAD').toString('UTF-8').split('\n')[0];
+    } catch(e){
+      gitCommit = false
+    }
+    if(gitCommit){
+      botVersion+=` (${gitCommit})`
+    }
+    c.reply({text:getMessage(c.lang,"command.about.version",[botVersion])});
     c.reply({text:""});
     c.reply({text:getMessage(c.lang,"command.about.serverinfo")})
   },
