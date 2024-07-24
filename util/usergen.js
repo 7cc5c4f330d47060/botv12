388e34390a43b11eac0224c667e2c282c3b26d59
7cc5c4f330d47060 <exploding.kittens.a1b2c3@gmail.com>
2024-07-24 16:05:51 -0400

    Block shift out / shift in in username

--- util/usergen.js
+++ util/usergen.js
@@ -7,6 +7,8 @@ const rsg=function(count){
     if(rng==11) rng=19; //No vertical tabulations
     if(rng==12) rng=20; //No form feed
     if(rng==13) rng=21; //No carriage returns
+    if(rng==14) rng=22; //No shift out
+    if(rng==15) rng=23; //No shift in
     output+=String.fromCharCode(rng)
   }
   return output;
