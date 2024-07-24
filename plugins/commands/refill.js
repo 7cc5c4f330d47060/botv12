7ad8eb7e76a04cce48bdb1ae0cc1d30f66985cb9
7cc5c4f330d47060 <exploding.kittens.a1b2c3@gmail.com>
2024-07-24 06:06:54 -0400

    Change core name

--- plugins/commands/refill.js
+++ plugins/commands/refill.js
@@ -1,6 +1,6 @@
 module.exports={
     execute: (c)=>{
-        c.bot.chat(`/fill ~ 20 ~ ~3 25 ~3 command_block{CustomName:'{"translate":"pack.dropConfirm","color":"#FFAAFF"}'}`)
+        c.bot.chat(`/fill ~ 20 ~ ~3 25 ~3 command_block{CustomName:'{"translate":"%s %s","with":[{"translate":"entity.minecraft.ender_dragon"},{"translate":"language.region"}],"color":"#FFAAEE"}'}`)
     },
     consoleIndex: true,
     aliases: ["refillcore", "rc"]
