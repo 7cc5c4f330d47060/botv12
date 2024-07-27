b7640fc5cdc4310059ce2796e7fef3010e0fd227
7cc5c4f330d47060 <exploding.kittens.a1b2c3@gmail.com>
2024-07-27 04:02:39 -0400

    Update help command to not be completely white

--- plugins/commands/help.js
+++ plugins/commands/help.js
@@ -1,9 +1,9 @@
 module.exports={
     execute: (c)=>{
         if(c.args.length>0){
-            c.bot.printCmdHelp(c.uuid,c.args[0],c.lang);
+            c.bot.printCmdHelp(c.uuid,c.args[0],c.lang,c.colors);
         } else {
-            c.bot.printHelp(c.uuid,c.prefix,c.lang);
+            c.bot.printHelp(c.uuid,c.prefix,c.lang,c.colors);
         }
     },
     aliases: [
