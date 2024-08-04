3f7c93a0733e8640ac052df50886eebc0caff79d
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2024-08-04 03:13:12 -0400

    Spacing [6]

--- plugins/command.js
+++ plugins/command.js
@@ -3,7 +3,6 @@ const hashcheck = require('../util/hashcheck.js')
 const settings = require('../settings.json')
 const { getMessage } = require('../util/lang.js')
 const cmds = require('../util/commands.js')
-
 module.exports = {
   load: (b) => {
     b.prefix = settings.prefix
