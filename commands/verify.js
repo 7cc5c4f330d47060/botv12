cd6ee042ec6278d68576ef1ed1125ebac83506b3
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2024-08-04 01:55:45 -0400

    Rewrite verify command

--- commands/verify.js
+++ commands/verify.js
@@ -1,10 +1,19 @@
+const {getMessage} = require('../util/lang.js')
 module.exports = {
   execute: (c) => {
+    const permsN = getMessage(c.lang, 'command.help.permsNormal')
+    const permsT = getMessage(c.lang, 'command.help.permsTrusted')
+    const permsO = getMessage(c.lang, 'command.help.permsOwner')
+    const permsC = getMessage(c.lang, 'command.help.permsConsole')
     c.reply({
-      text: c.verify + ''
-    })
-    c.reply({
-      text: c.command
+      translate: getMessage(c.lang, "command.verify.success"),
+      color: c.colors.secondary,
+      with: [
+        {
+          text: [permsN, permsT, permsO, permsC][c.verify],
+          color: c.colors.primary
+        }
+      ]
     })
   },
   aliases: ['validate'],
