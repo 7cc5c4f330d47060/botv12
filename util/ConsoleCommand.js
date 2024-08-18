648176086040fe2816a40cc82c8f890978ac38e4
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2024-08-18 03:17:47 -0400

    Remove extraneous comments

--- util/ConsoleCommand.js
+++ util/ConsoleCommand.js
@@ -1,17 +1,16 @@
-// HOW TO WRITE CLASS JS
 const index = require('../index.js')
 const parse = require('../util/chatparse_console.js')
 const settings = require('../settings.json')
 class ConsoleCommand {
   constructor (cmd, index2) {
-    this.send = () => {} // not needed for console
+    this.send = () => {}
     this.reply = text => process.stdout.write(parse(text) + '\n')
-    this.uuid = 'dde5a2a6-ebdd-7bbb-8eac-f75b10c10446' // hard-coded because uuid does not exist at console
+    this.uuid = 'dde5a2a6-ebdd-7bbb-8eac-f75b10c10446'
     this.username = 'Owner'
     this.nickname = 'Console'
     this.command = cmd
     this.msgType = "_bot_console";
-    this.prefix = '' // prefix does not exist at console
+    this.prefix = ''
     this.bot = index2 >= 0
       ? index.bot[index2]
       : {}
@@ -19,7 +18,7 @@ class ConsoleCommand {
     this.args = cmd.split(' ').slice(1)
     this.verify = 3
     this.host = ''
-    this.port = '3' // :3
+    this.port = '3'
     this.lang = settings.defaultLang
     this.colors = settings.colors
   }
