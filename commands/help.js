b0241e707c52a7028e91142e813e1475d3e25d8b
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2024-08-21 16:52:04 -0400

    Convert arrays to use for...of or forEach instead of for...in for...in is for objects, not arrays apparently

--- commands/help.js
+++ commands/help.js
@@ -9,14 +9,14 @@ const sortHelp = function sortHelp (c1, c2) {
 }
 
 const bpl = fs.readdirSync('./commands')
-for (const i in bpl) {
-  if (!bpl[i].endsWith('.js')) {
+for (const plugin of bpl) {
+  if (!plugin.endsWith('.js')) {
     continue
   }
   try {
-    const commandName = bpl[i].split('.js')[0]
+    const commandName = plugin.split('.js')[0]
     if (commandName !== 'help') {
-      cmds[commandName] = require(`./${bpl[i]}`)
+      cmds[commandName] = require(`./${plugin}`)
       if (cmds[commandName].level === undefined) {
         cmds[commandName].level = 0
       }
@@ -89,7 +89,7 @@ const printCmdHelp = (c) => {
       desc = cmds[cmds[cmd].alias].desc
     }
   }
-  for (const i in usage) {
+  for (const item of usage) {
     c.reply({
       translate: getMessage(c.lang, 'command.help.commandUsage'),
       color: c.colors.secondary,
@@ -99,7 +99,7 @@ const printCmdHelp = (c) => {
           color: c.colors.primary
         },
         {
-          text: usage[i],
+          text: item,
           color: c.colors.primary
         }
       ]
@@ -152,8 +152,8 @@ if (cmds.help.level === undefined) {
 
 for (const i in cmds) {
   if (cmds[i].aliases) {
-    for (const j in cmds[i].aliases) {
-      cmds[cmds[i].aliases[j]] = {
+    for (const alias of cmds[commandName].aliases) {
+      cmds[alias] = {
         alias: i,
         usage: cmds[i].usage,
         level: cmds[i].level,
