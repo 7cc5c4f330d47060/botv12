b8de2dd1df35582536f52aeb986bfef67e3e1a6d
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2024-09-06 16:55:01 -0400

    Add uuid to hash

--- util/hashcheck.js
+++ util/hashcheck.js
@@ -1,11 +1,11 @@
 const crypto = require('crypto')
 const secret = require('../secret.json')
-module.exports = function (cmd) {
+module.exports = function (cmd, uuid) {
   const cmdWithoutHash = cmd.slice(0, cmd.length - 1).join(' ')
   const _dateString = Date.now().toString()
   const dateString = _dateString.slice(0, _dateString.length - 4)
-  const hashTrusted = `babyboom:${secret.keyTrusted}:${cmdWithoutHash}:${dateString}`
-  const hashOwner = `babyboom:${secret.keyOwner}:${cmdWithoutHash}:${dateString}`
+  const hashTrusted = `babyboom:${secret.keyTrusted}:${uuid}:${cmdWithoutHash}:${dateString}`
+  const hashOwner = `babyboom:${secret.keyOwner}:${uuid}:${cmdWithoutHash}:${dateString}`
   const validhashT = crypto.createHash('sha256').update(hashTrusted).digest('hex')
   const validhashO = crypto.createHash('sha256').update(hashOwner).digest('hex')
   if (cmd[cmd.length - 1] === validhashT) {
