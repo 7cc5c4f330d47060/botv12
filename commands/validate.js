12f6bcdc3675205aa8e7ebe9c120c1b41c2dfc63
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2024-09-06 16:59:16 -0400

    Remove console permission level

--- commands/validate.js
+++ commands/validate.js
@@ -4,13 +4,12 @@ module.exports = {
     const permsN = getMessage(c.lang, 'command.help.permsNormal')
     const permsT = getMessage(c.lang, 'command.help.permsTrusted')
     const permsO = getMessage(c.lang, 'command.help.permsOwner')
-    const permsC = getMessage(c.lang, 'command.help.permsConsole')
     c.reply({
       translate: getMessage(c.lang, 'command.verify.success'),
       color: c.colors.secondary,
       with: [
         {
-          text: [permsN, permsT, permsO, permsC][c.verify],
+          text: [permsN, permsT, permsO][c.verify],
           color: c.colors.primary
         }
       ]
