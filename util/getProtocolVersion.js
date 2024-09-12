9847623b254d8151e38e9bc6a6157c82ba6941a3
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2024-09-12 00:17:46 -0400

    Add protocol version getter

--- util/getProtocolVersion.js
+++ util/getProtocolVersion.js
@@ -1,4 +1,4 @@
 const mcd=require("minecraft-data")
 module.exports = function (ver) {
-  return require("minecraft-data").versionsByMinecraftVersion.pc[ver].version
+  return mcd.versionsByMinecraftVersion.pc[ver].version
 }
