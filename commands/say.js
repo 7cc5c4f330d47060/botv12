a792fe42898b1f2caa7bdf069f4be01bcfb001d7
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2024-09-19 22:21:01 -0400

    Lint

--- commands/say.js
+++ commands/say.js
@@ -1,7 +1,7 @@
 module.exports = {
   execute: (c) => {
     if (c.args[0].startsWith('/') && c.verify < 1) return
-    c.bot.chat(c.args.join(' ').slice(0,512))
+    c.bot.chat(c.args.join(' ').slice(0, 512))
   },
   consoleIndex: true,
   aliases: ['echo']
