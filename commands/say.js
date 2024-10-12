a3835de35c23aa0e6aef536d7b75df64d37f5799
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2024-10-12 01:33:00 -0400

    Say command improvements

--- commands/say.js
+++ commands/say.js
@@ -1,6 +1,40 @@
+const settings = require('../settings.json')
+const version = require('../version.json')
 module.exports = {
   execute: (c) => {
-    if (c.args[0].startsWith('/') && c.verify < 1) return
+    if (c.verify < 1){
+      c.bot.tellraw("@a",{
+        translate: "%s %s: %s",
+        color: "white",
+        with: [
+          {
+            translate: '[%s]',
+            color: 'white',
+            with: [
+              {
+                translate: '%s: %s',
+                color: settings.colors.secondary,
+                with: [
+                  {
+                    text: 'Prefix'
+                  },
+                  {
+                    text: c.bot.prefix[0],
+                    color: settings.colors.primary
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            text: version.botName,
+            color: settings.colors.primary
+          },
+          c.args.join(' ').slice(0, 512)
+        ]
+      })
+      return
+    }
     c.bot.chat(c.args.join(' ').slice(0, 512))
   },
   consoleIndex: true,
