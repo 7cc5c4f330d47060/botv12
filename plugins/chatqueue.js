58ab6efb96f758ed1a815f5191ba7a646291ecb9
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2024-10-20 19:02:13 -0400

    Add chat queue

---
+++ plugins/chatqueue.js
@@ -0,0 +1,19 @@
+const load = (b) => {
+  b._client.on('login', () => {
+    b.interval.chatQueue = setInterval(() => {
+      if (b.chatqueue.length !== 0) {
+        b._client.chat(b.chatqueue[0])
+        b.chatqueue.splice(0, 1)
+      }
+    }, 100)
+  })
+  b.matcherRegex = /.{1,255}/g
+  b.chatqueue = ["uwu (ignore this, it is for testing)","owo (ignore this, it is for testing)"]
+  b.chat = function chat (msg) {
+    if (msg.length === 0) return
+    msg.match(b.matcherRegex).forEach(element => {
+      b.chatqueue.push(element)
+    })
+  }
+}
+export { load }
