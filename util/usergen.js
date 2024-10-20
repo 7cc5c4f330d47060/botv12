9462cb603cabe4efcefe3f6b3f11d81590ac9ab5
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2024-10-20 14:51:31 -0400

    Fix username generator

--- util/usergen.js
+++ util/usergen.js
@@ -1,5 +1,5 @@
 import { randomBytes } from "crypto";
 const generateUser = function (legal) {
-  return `${parseInt(randomBytes(8).toString("hex"),36)}`
+  return `${parseInt(randomBytes(4).toString("hex"),16).toString(36)}${parseInt(randomBytes(4).toString("hex"),16).toString(36)}`
 }
 export { generateUser }
