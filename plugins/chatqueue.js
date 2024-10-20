e4ec458e6e85a40b8fc8295e7619ddcaca3d6ea2
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2024-10-20 19:02:55 -0400

    Remove testing entries from chat queue

--- plugins/chatqueue.js
+++ plugins/chatqueue.js
@@ -8,7 +8,7 @@ const load = (b) => {
     }, 100)
   })
   b.matcherRegex = /.{1,255}/g
-  b.chatqueue = ["uwu (ignore this, it is for testing)","owo (ignore this, it is for testing)"]
+  b.chatqueue = []
   b.chat = function chat (msg) {
     if (msg.length === 0) return
     msg.match(b.matcherRegex).forEach(element => {
