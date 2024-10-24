0b8ea66cba749b332748171896cba1d54e3a746d
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2024-10-23 22:59:54 -0400

    Export default

--- util/uuidtoint.js
+++ util/uuidtoint.js
@@ -1,4 +1,4 @@
-export function uuidToInt (uuid) {
+export default function uuidToInt (uuid) {
   const splitUUID = uuid.replace(/[^0-9a-f]/g, '').replace(/.{1,8}/g, a => { return `0x${a}` }).match(/.{1,10}/g)
   const numUUID = [+splitUUID[0] << 0, +splitUUID[1] << 0, +splitUUID[2] << 0, +splitUUID[3] << 0]
   return numUUID
