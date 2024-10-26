0bb0d6f16852e3d30cdadb7adb994f67735860c6
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2024-10-26 13:32:34 -0400

    Fix aliases

--- util/commands.js
+++ util/commands.js
@@ -1,7 +1,8 @@
 const cmds = Object.create(null)
 
-const bpl = readdirSync('commands')
 import { readdirSync } from "node:fs"
+const bpl = readdirSync('commands')
+
 for (const plugin of bpl) {
   if (!plugin.endsWith('.js')) {
     continue
@@ -10,23 +11,20 @@ for (const plugin of bpl) {
     const commandName = plugin.split('.js')[0]
     import(`../commands/${plugin}`).then((pluginItem)=>{
         cmds[commandName] = pluginItem // For rejoining
+        if (cmds[commandName].aliases) {
+          for (const j in cmds[commandName].aliases) {
+            cmds[cmds[commandName].aliases[j]] = {
+              execute: cmds[commandName].execute,
+              alias: commandName,
+              usage: cmds[commandName].usage,
+              level: cmds[commandName].level,
+              hidden: true,
+              consoleIndex: cmds[commandName].consoleIndex
+            }
+          }
+        }
     })
   } catch (e) { console.log(e) }
-}
-
-for(const commandName in cmds){
-  if (cmds[commandName].aliases) {
-    for (const j in cmds[commandName].aliases) {
-      cmds[cmds[commandName].aliases[j]] = {
-        execute: cmds[commandName].execute,
-        alias: commandName,
-        usage: cmds[commandName].usage,
-        level: cmds[commandName].level,
-        hidden: true,
-        consoleIndex: cmds[commandName].consoleIndex
-      }
-    }
-  }
 }
 
 export default cmds
