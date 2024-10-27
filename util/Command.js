780e570fc0ae05a33fcd17bb10e84fee4825a21f
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2024-10-27 01:23:28 -0400

    Add more to settiongs

--- util/Command.js
+++ util/Command.js
@@ -5,10 +5,15 @@ export default class Command {
         this.reply = text => bot.tellraw(uuid, text)
         this.username = user;
         this.nickname = nick;
+        this.command = cmd;
+        this.type = senderType;
+        this.msgType = msgType;
+        this.msgSubtype = msgSubtype; 
         this.args = cmd.split(' ').slice(1)
         this.cmdName = cmd.split(' ')[0]
         this.prefix = prefix
         this.colors = settings.colors
+        this.lang = settings.defaultLang
         this.verify = verify
         this.host = bot.host.host
         this.port = bot.host.port
