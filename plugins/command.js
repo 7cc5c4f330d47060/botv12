d8151756e32d7dfa0d675e284c525ef52822b769
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2024-11-07 01:22:04 -0500

    Adjustments to the command handler

--- plugins/command.js
+++ plugins/command.js
@@ -12,7 +12,10 @@ export default function load (b) {
     }
   })
   b.runCommand = function (user, nick, uuid, command, type, subtype, prefix){
-    const context = new Command(uuid, user, nick, command, "minecraft", type, subtype, prefix, b, 0, {})
+    const context = new Command(uuid, user, nick, command, "minecraft", type, subtype, prefix, b, 0)
+
+    b.emit('command', context)
+
     if(cmds[context.cmdName.toLowerCase()]){
       try {
         cmds[context.cmdName.toLowerCase()].execute(context)
