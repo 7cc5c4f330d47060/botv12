69aa860e96fd8d1e46171fd658c300cba56c393c
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2024-11-19 20:14:40 -0500

    Update example settings

--- settings_example.js
+++ settings_example.js
@@ -1,9 +1,8 @@
 export default {
-  terminalMode: 'blackTerminal_24bit',
-  version_mc: '1.21.1',
-  displaySubtypesToConsole: true,
-  defaultLang: 'en-US',
-  colors: {
+  terminalMode: 'blackTerminal_24bit', // Terminal mode. Most modern terminals support 24-bit color
+  version_mc: '1.21.1', // Minecraft version to connect with
+  defaultLang: 'en-US', // Default language
+  colors: { // All colors the bot uses
     secondary: '#DD99FF',
     primary: '#EECCFF',
     tertiary: 'white',
@@ -11,18 +10,18 @@ export default {
     error: '#FF6688',
     fatalError: '#BB3344'
   },
-  prefixes: [
+  prefixes: [ // A list of prefixes the bot will respond to in-game
     'ubotesm:',
     'ubotdev:',
     'es"',
     'd"'
   ],
-  servers: [
+  servers: [ // Server list
     {
-      host: 'kaboom.pw',
-      port: 25565,
+      host: 'kaboom.pw', // Server address
+      port: 25565, // Server port. The default is 25565.
       options: {
-        name: 'kaboom'
+        name: 'kaboom' // Server name in netmsg
       }
     },
     {
