07a1c0754c59ff2aac9f6b998916181b05b9f499
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2024-11-20 01:35:16 -0500

    Lint

--- commands/cb.js
+++ commands/cb.js
@@ -1,6 +1,6 @@
-const execute =  (c) => {
+const execute = (c) => {
   c.bot.ccq.push(c.args.join(' '))
 }
-const consoleIndex =  true
-const aliases =  ['commandblock', 'cmdblock']
+const consoleIndex = true
+const aliases = ['commandblock', 'cmdblock']
 export { execute, consoleIndex, aliases }
--- commands/stop.js
+++ commands/stop.js
@@ -1,7 +1,7 @@
-const execute= (c) => {
+const execute = (c) => {
   process.exit(1)
 }
 const aliases = ['exit']
-const level= 2
+const level = 2
 
 export { execute, aliases, level }
