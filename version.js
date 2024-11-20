455be884888ff9e07f1f073a1762fe4b46291e1f
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2024-11-20 02:00:34 -0500

    Madeline :3

--- version.js
+++ version.js
@@ -1,6 +1,6 @@
 export default {
   botName: 'botv12 Dev',
-  botVersion: '12.0.0-alpha.1',
+  botVersion: '12.0.0-alpha.2',
   botAuthor: 'owo439895035',
   isPreRelease: true,
   sourceURL: 'https://code.chipmunk.land/7cc5c4f330d47060/botv12'
