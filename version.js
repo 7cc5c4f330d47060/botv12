c784be321d5f169027f987e19eab97927dc75395
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2024-11-20 02:12:59 -0500

    Add comments

--- version.js
+++ version.js
@@ -1,7 +1,7 @@
 export default {
-  botName: 'botv12 Dev',
-  botVersion: '12.0.0-alpha.2',
-  botAuthor: 'owo439895035',
-  isPreRelease: true,
-  sourceURL: 'https://code.chipmunk.land/7cc5c4f330d47060/botv12'
+  botName: 'botv12 Dev', // Name of the bot
+  botVersion: '12.0.0-alpha.2', // Version of the bot. This is different from the one in package.json, which has to be updated seperately.
+  botAuthor: 'owo439895035', // The creator of the bot
+  isPreRelease: true, // If this version is a pre-release version. Used to show a warning in the about command.
+  sourceURL: 'https://code.chipmunk.land/7cc5c4f330d47060/botv12' // Source code repository URL. This is different from the one in package.json.
 }
