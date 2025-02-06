d17d2a6add136ad0572a1d9850927dffc2c190a1
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2025-02-06 00:34:04 -0500

    Add online mode

--- lang/en-US.js
+++ lang/en-US.js
@@ -127,5 +127,8 @@ export default {
   'command.disabled.local': 'This command has been disabled on this server.',
   'command.disabled.console': 'This command cannot be run from the console.',
   'command.disabled.nonConsole': 'This command must be run from the console.',
-  copyText: 'Click to copy this item to your clipboard'
+  copyText: 'Click to copy this item to your clipboard',
+
+  'command.filter.warning': 'This command is only to serve as a reference implementation for player management features, whether it be for forks of the botv12 / UBot codebase, or completely different bots. This will be disabled by default in the example settings file, with a toggle in settings which can be enabled if the owner of the bot so desires. On the main UBot production instance, these features will be entirely disabled, so it won\'t even run from console. If the bot\'s owner chooses to enable these, they are responsible for obtaining exploits to use with the feature, and any outcome from usage of these features, whether it be positive or negative in nature.'
+
 }
