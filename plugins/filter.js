52fa338dbdb04c656a05129b978a603dfb03f471
Your Name <you@example.com>
2025-02-09 20:19:06 -0500

    Changes to filter command, add player scaling command (it changes several player attributes to resize the player and make physics work correctly, still not perfect)

--- plugins/filter.js
+++ plugins/filter.js
@@ -1,13 +1,3 @@
-/*
-  The code contained within this file is only to serve as reference implementations for player
-  management features, whether it be for forks of the botv12 / UBot codebase, or completely
-  different bots. This will be disabled by default in the example settings file, with a toggle in
-  settings which can be enabled if the owner of the bot so desires. On the main UBot production
-  instance, these features will be entirely disabled, so it won't even run from console. If the
-  bot's owner chooses to enable these, they are responsible for obtaining exploits to use with the
-  feature, and any outcome from usage of these features, whether it be positive or negative in
-  nature.
-*/
 export default function load (b) {
 
 }
