b8e460fdecf6d1364a0adab2d1beeafd08f8cde7
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2025-02-11 20:58:35 -0500

    Test update 1

--- plugins/chunk.js
+++ plugins/chunk.js
@@ -1,4 +1,17 @@
+import { default as c_loader } from 'prismarine-chunk';
+const c = c_loader("1.18")
+import { default as w_loader }  from 'prismarine-world';
+const w = w_loader()
+const rd = 4;
+
+
 //Chunk data parsing placeholder, for core
 export default function load (b) {
-    //b._client.on("map_chunk", payload => console.log(payload))
+    
+    b.worlds={}
+    b._client.on("map_chunk", payload => {
+        //console.log(payload);
+        b._chunk = new c();
+        b._chunk.load(payload.chunkData)
+    })
 }
