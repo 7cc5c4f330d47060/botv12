8b28d2cbba201356a8da0966211e666107aeafba
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2025-02-12 16:04:02 -0500

    w

--- util/memoryconvert.js
+++ util/memoryconvert.js
@@ -1,16 +1,16 @@
-const UNITS = ['B', 'KB', 'MB', 'GB', 'TB', 'PB'];
-const THRESHOLD = 1000; // KiB = 1024; KB = 1000
+const UNITS = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
+const THRESHOLD = 1000 // KiB = 1024; KB = 1000
 
 export default function memoryconvert (bytes) {
   for (let i = 0; i < UNITS.length; i++) {
-    const last = i === (UNITS.length - 1);
-    const max = THRESHOLD ** (i + 1);
-    if (!last && bytes >= max) continue;
+    const last = i === (UNITS.length - 1)
+    const max = THRESHOLD ** (i + 1)
+    if (!last && bytes >= max) continue
 
-    const divisor = THRESHOLD ** i;
-    const unit = UNITS[i];
+    const divisor = THRESHOLD ** i
+    const unit = UNITS[i]
 
-    const div = bytes / divisor;
+    const div = bytes / divisor
     return `${+div.toFixed(2)} ${unit}`
   }
 }
