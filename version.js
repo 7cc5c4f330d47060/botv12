ef09fbe880d21ecb9fe727b81e0d1797f7bf152e
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2025-04-19 00:07:11 -0400

    Change version

--- version.js
+++ version.js
@@ -1,6 +1,6 @@
 export default {
   botName: 'botv12 Dev', // Name of the bot
-  botVersion: '12.0.0-alpha.2', // Version of the bot. This is different from the one in package.json, which has to be updated seperately.
+  botVersion: '12.0.0-alpha.3', // Version of the bot. This is different from the one in package.json, which has to be updated seperately.
   botAuthor: 'owo439895035', // The creator of the bot
   isPreRelease: true, // If this version is a pre-release version. Used by the about command to show a warning if set to true.
   sourceURL: 'https://code.chipmunk.land/7cc5c4f330d47060/botv12', // Source code repository URL. This is different from the one in package.json.
