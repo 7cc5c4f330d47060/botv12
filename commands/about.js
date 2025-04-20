4acfd1f6810a86ec049c7d435d7f018117c4e0f7
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2025-04-20 04:28:55 -0400

    Sort commands in help alphabetically

--- commands/about.js
+++ commands/about.js
@@ -1,11 +1,4 @@
-/*
-  If you are forking the bot, you must comply with the MIT License, which only requires
-  attribution. If you choose to remove or modify this command, you must find another way to comply
-  with the MIT License, such as adding a message on join or in another command.
-  You may also make your fork open source as well, and add a message in the readme.
-  Additionally, if you're forking a fork with a different license, such as GPL, make sure you
-  comply with any additional terms.
-*/
+
 
 import aboutServer from './aboutSub/server.js'
 import displayServerList from './aboutSub/serverList.js'
