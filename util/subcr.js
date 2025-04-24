5287c97c5b3ed21250115391657188a454e9b977
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2025-04-23 20:38:04 -0400

    Make the about command use a registry of commands

---
+++ util/subcr.js
@@ -0,0 +1,33 @@
+import CommandR from './cr.js'
+export default class SubCommandR extends CommandR {
+  constructor () {
+    super()
+    
+    this.aliases = [];
+
+    this.runCommand = function (context){
+      const args = context.args
+      let subCommand = args[0]
+      const command = this.getCommand(subCommand)
+      if(command) {
+        args.splice(0, 1)[0]
+        context.rewriteCommand(args.join(" "))
+      } else {
+        subCommand = 'base'
+      }
+      this.getCommand(subCommand).execute(context)
+    }
+
+    this.register = function (name, payload, aliases) {
+      if(name !== 'base') this.aliases.push(name)
+      const command = {}
+      command.execute = payload
+      if (aliases) for (const alias of aliases){
+        this._aliases[alias] = name
+        this.aliases.push(alias)
+      }
+      command.aliases = aliases
+      this._commands[name] = command
+    }
+  }
+}
