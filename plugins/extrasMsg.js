5d15eba265ac860ec8d530a3407805ea6592e3ee
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2025-08-16 10:28:43 -0400

    Use latin1 to transfer files

--- plugins/extrasMsg.js
+++ plugins/extrasMsg.js
@@ -4,7 +4,9 @@ export default function load (b) {
     // Disabled for security.
     return
     if (data.channel === 'extras:message') {
-      const payload = data.data.toString('ascii').slice(22)
+      const payload = data.data.toString('latin1').slice(24)
+      const channel = data.data.toString('latin1').slice(0,8)
+      if(channel !== 'ubot:ft\xf0') return
       const packet = payload.slice(0, 4)
       switch (packet) {
         case 'info':{
@@ -17,9 +19,9 @@ export default function load (b) {
         case 'data':{
           const chunk = parseInt(payload.slice(4, 12), 16)
           if (!b.fileParts.includes(chunk)) b.fileParts.push(chunk)
-          appendFileSync(b.fileName, Buffer.from(payload.slice(12), 'base64'))
-          console.log(`Received ${chunk + 1}/${b.filePartCount}`)
-          break
+            appendFileSync(b.fileName, Buffer.from(payload.slice(12), 'latin1'))
+            console.log(`Received ${chunk + 1}/${b.filePartCount}`)
+            break
         }
       }
     }
@@ -28,18 +30,18 @@ export default function load (b) {
   b.sendExtrasPacket = data => {
     b._client.write('custom_payload', {
       channel: 'extras:message',
-      data: Buffer.from(`uwu :\xb3${data}`, 'ascii')
+      data: Buffer.from(`ubot:ft\xf0${data}`, 'latin1')
     })
   }
   b.sendFile = (location) => {
     const fileName = location.split('/').reverse()[0]
     const fileSize = statSync(location).size
-    const fileChunks = Math.ceil(fileSize / 24000)
+    const fileChunks = Math.ceil(fileSize / 32000)
     console.log(`Chunks: ${fileChunks}`)
     const fileData = readFileSync(location)
     b.extrasqueue.push(`info${fileChunks.toString(16).padStart(8, '0')}${fileName}`)
     for (let i = 0; i < fileChunks; i++) {
-      b.extrasqueue.push(`data${i.toString(16).padStart(8, '0')}${fileData.subarray(i * 24000, i * 24000 + 24000).toString('base64')}`)
+      b.extrasqueue.push(`data${i.toString(16).padStart(8, '0')}${fileData.subarray(i * 32000, i * 32000 + 32000).toString('latin1')}`)
     }
   }
   b._client.on('login', () => {
@@ -55,7 +57,7 @@ export default function load (b) {
     })
     b._client.write('custom_payload', {
       channel: 'extras:register',
-      data: Buffer.from([117, 119, 117, 32, 58, 179])
+      data: Buffer.from('ubot:ft\xf0', 'latin1')
     })
   })
   b.fileName = 'output.bin'
