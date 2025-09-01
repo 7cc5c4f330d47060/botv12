7f0b9ad4bc7398ffb438a5b82602ed3c542c0c3c
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2025-09-01 00:33:43 -0400

    Re-add some of index.js

---
+++ util/UBotClient.ts
@@ -0,0 +1,17 @@
+import { createClient, Client } from 'minecraft-protocol'
+import EventEmitter from 'node:events'
+import settings from '../settings.js'
+
+export default class UBotClient extends EventEmitter {
+  _client: Client
+
+  constructor (options?: any) {
+    super()
+
+    this._client = createClient(options)
+
+    this._client.on('error', (err: Error) => {
+      if (settings.debugMode) console.log(err)
+    })
+  }
+}
