4e01d49130b06b06f791bb7c8aee44c4503ec8bd
Your Name <you@example.com>
2025-09-12 19:52:08 -0400

    Change to 2020 because some logger code is from botv2

--- version.js
+++ version.js
@@ -3,7 +3,7 @@ export default {
   botVersion: '12.0.0-alpha.5', // Version of the bot. This is different from the one in package.json, which has to be updated seperately.
   botAuthor: 'owo439895035', // The creator of the bot
   isPreRelease: true, // If this version is a pre-release version. Used by the about command to show a warning if set to true.
-  copyrightYear: '2024-2025', // Years that the bot was developed in. Should be a string.
+  copyrightYear: '2020-2025', // Years that the bot was developed in. Should be a string.
   sourceURL: 'https://codeberg.org/7cc5c4f330d47060/botv12', // Source code repository URL. This is different from the one in package.json.
   originalRepo: 'https://codeberg.org/7cc5c4f330d47060/botv12',
   originalName: 'botv12',
