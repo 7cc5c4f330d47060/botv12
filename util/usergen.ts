a97197754e571dbe6672bd50a85c8002c3a1ff6f
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2025-09-29 00:20:05 -0400

    Change name format

--- util/usergen.ts
+++ util/usergen.ts
@@ -1,5 +1,29 @@
-import { randomBytes } from 'crypto'
+import { createHash } from 'crypto'
 
+const names = [ // From 2017 Metroidvania from Australia
+  // HC
+  'Fungus1_28',
+  // RG
+  'Crossroads_46b',
+  'Crossroads_50',
+  'Ruins2_10',
+  // City of Tears
+  'Crossroads_49b',
+  // East Deepnest
+  'Abyss_03_c',
+  // West Deepnest
+  'Abyss_03_b',
+  'Fungus2_25',
+  // Waterways
+  'Abyss_01',
+  'Abyss_02',
+  // Gardens
+  'Deepnest_43',
+  // Wastes
+  'Deepnest_01',
+  // Crossroads
+  'Mines_33',
+]
 export default function generateUser (): string{
-  return randomBytes(8).toString('hex')
+  return `${createHash('sha256').update(names[Math.floor(Math.random()*names.length)]).digest('hex').slice(0,6)}`
 }
