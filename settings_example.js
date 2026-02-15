fca250ac0ec3d851f04aa05026efd61d88c94b65
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2026-02-14 22:42:39 -0500

    Change default version to 1.21.11

--- settings_example.js
+++ settings_example.js
@@ -1,6 +1,6 @@
 export default {
   terminalMode: 'blackTerminal_24bit', // Terminal mode. Most modern terminals support 24-bit color
-  version_mc: '1.21.10', // Minecraft version to connect with
+  version_mc: '1.21.11', // Minecraft version to connect with
   defaultLang: 'en-US', // Default language
   /*
     If you plan to use a database with the bot, set dbEnabled to true and fill in the below fields.
