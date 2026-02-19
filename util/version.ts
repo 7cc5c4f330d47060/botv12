72801f8ec33ff72f9d6526f3affe0df83c9a3fcf
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2026-02-18 21:08:45 -0500

    Change version format on version.ts

--- util/version.ts
+++ util/version.ts
@@ -3,11 +3,15 @@ import { execSync } from 'child_process'
 import { readdirSync } from 'node:fs'
 
 let botVersion = version.botVersion
-
+let versionString = `${botVersion.major}.${botVersion.minor}.${botVersion.patch}`
+if (botVersion.prType) {
+  versionString += `-${botVersion.prType}.${botVersion.prMinor}`
+  if(botVersion.prPatch) versionString += `.${botVersion.prPatch}`
+}
 if(readdirSync(baseDir).includes('.git')) {
   const gitCommit = execSync(`git -C ${baseDir} rev-parse --short HEAD`).toString('utf8').split('\n')[0]
   const gitBranch = execSync(`git -C ${baseDir} rev-parse --abbrev-ref HEAD`).toString('utf8').split('\n')[0]
-  botVersion += ` (${gitCommit} - ${gitBranch})`
+  versionString += ` (${gitCommit} - ${gitBranch})`
 }
 
-export default botVersion
+export default versionString
--- version.ts
+++ version.ts
@@ -1,7 +1,16 @@
 const version = {
   botName: 'botv12 Dev', // Name of the bot
   botCodename: 'botv12',
-  botVersion: '(main branch)', // Version of the bot. Different from package.json.
+  botVersion: {
+    major: 1,
+    minor: 0,
+    patch: 0,
+    //prMajor: 0,
+    prMinor: 6,
+    prPatch: 0,
+    prType: 'alpha',
+    isMainLine: true
+  },
   botAuthor: '7cc5c4f330d47060', // The creator of the bot
   isPreRelease: true, // If this version is a pre-release version.
   settingsFormat: 2,
