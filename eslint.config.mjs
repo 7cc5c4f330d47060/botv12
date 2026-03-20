238c5409dc8de36a7645c3a755477067ab922dba
7cc5c4f330d47060 <samsungipadistaken@outlook.com>
2026-03-20 05:24:26 -0400

    Fix more eslint errors (16 left)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,11 @@
 import neostandard from 'neostandard'
 
 export default neostandard({
-  ignores: ['dist/**/*', 'data/**/*'],
+  ignores: [
+    'dist/**/*',
+    'data/**/*',
+    'util/ubot-panel/**/*' /* Temporary */
+  ],
   env: ['browser', 'node'],
   ts: true
 })
